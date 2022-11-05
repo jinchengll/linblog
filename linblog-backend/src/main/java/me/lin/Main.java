5d70d4e551289d38e1c8c43e9d2ee0d2abebfcae
jinchengll <jinchengll@qq.com>
2022-11-05 22:23:20 +0800

    init：初始化后端项目

---
+++ linblog-backend/src/main/java/me/lin/Main.java
@@ -0,0 +1,12 @@
+package me.lin;
+
+/**
+ * @author jinchengll
+ * @version 1.0
+ * @date ${DATE} ${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ linblog-backend/src/test/java/me/lin/Test.java
@@ -0,0 +1,9 @@
+package me.lin;
+
+/**
+ * @author jinchengll
+ * @version 1.0
+ * @date 2022/11/5 18:44
+ */
+public class Test {
+}
